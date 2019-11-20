f65ab6b89828338c0d608312295607a3df97483d
doaaalmuhaisen952 <almuhaisendoaa@gmail.com>
2019-11-20 18:33:43 +0200

    new

---
+++ app.js
@@ -0,0 +1,5 @@
+var name = ["raghad", "salma", "yamen", "Ahmad"];
+var age = ["23", "27", "23", "22"];
+
+document.getElementById("demo").innerHTML = name;
+document.getElementById("demo2").innerHTML = age;
